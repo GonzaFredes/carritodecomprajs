9e986b1d6d38b63e48f52fb979f0fa649613abdb
Gonzalo Fredes <gonzafredes1@gmail.com>
2022-09-12 20:14:39 -0300

    Actualizacion con funcion para carrito de compras y constructor de productos

--- index.js
+++ index.js
@@ -1,153 +1,73 @@
-//Se definen las variables iniciales,se le pide al usuario que ingrese su nombre y que ingrese el tipo de producto que está buscando comprar.
-let totalCompra = 0
-let nombreCliente = prompt("Bienvenid@! \n \nAquí podrás adquirir todos nuestros productos\n \nPor favor ingresa tu nombre.")
-let productoSeleccionado = parseInt(prompt("Hola " + nombreCliente + " ¿Que quieres comprar hoy?\n\n1-Pantalones\n2-Remeras\n3-Calzado"))
-let seguirComprando = true
-let decision
 let productos = []
 let carrito = []
 
-//Ingreso los productos y sus características a travez de objetos y haciendo push de cada uno de ellos a la variable "productos"
-const pantalon1 = {
-    nombre: "Pantalon largo de jean Levis",
-    talle: "42",
-    cantidad: 5,
-    color: "gris",
-    precio: 1000,
+class Producto{
+    constructor(id, nombre, precio, img, desc = ''){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.img = img
+        this.desc = desc
+    }
+    desplegarProductos(){
+        const card = `
+            <div class="card">
+                <p>${this.nombre}</p>
+                <div>
+                    <img class='imagenProducto' src=${this.img} alt="foto del producto"/>
+                </div>
+                <div>
+                    <p>$${this.precio}</p>
+                </div>
+                <div class="btn-container">
+                    <button id=${this.id} class='btnAgregar'>AÑADIR AL CARRITO</button>
+                </div>
+            </div>
+        `
+        const container = document.getElementById('container')
+        container.innerHTML += card
+    }
+    agregarEvento(){
+        const btnAgregar = document.getElementById(this.id)
+        const productoEncontrado = productos.find(product => product.id == this.id)
+        btnAgregar.addEventListener('click', () => agregarAlCarrito(productoEncontrado))
+    }
 }
-productos.push(pantalon1) //0
-const pantalon2 = {
-    nombre: "Pantalon largo vestir Billabong",
-    talle: "40",
-    cantidad: 4,
-    color: "negro",
-    precio: 1200,
-}
-productos.push(pantalon2) //1
-const pantalon3 = {
-    nombre: "Pantalon largo de jean RipCurl",
-    talle: "41",
-    cantidad: 5,
-    color: "azul",
-    precio: 1830,
-}
-productos.push(pantalon3) //2
-const pantalon4 = {
-    nombre: "Pantalon corto skate RipCurl",
-    talle: "41",
-    cantidad: 5,
-    color: "azul",
-    precio: 1500,
-}
-productos.push(pantalon4) //3
-const remera1 = {
-    nombre: 'Remera estampada Reef',
-    talle: ["M", "L", "S"],
-    cantidad: 15,
-    color: ["blanco", "celeste", "negro"],
-    precio: 1150,
-}
-productos.push(remera1) //4
-const remera2 = {
-    nombre: 'Remera lisa Reef',
-    talle: ["M", "L", "S"],
-    cantidad: 15,
-    color: ["blanco", "rojo", "azul"],
-    precio: 1350,
-}
-productos.push(remera2) //5
-const remera3 = {
-    nombre: 'Remera lisa DC',
-    talle: ["M", "L", "S"],
-    cantidad: 15,
-    color: ["blanco", "negro"],
-    precio: 1000,
-}
-productos.push(remera3) //6
-const calzado1 = {
-    nombre: 'Zapatillas Nike AirMax',
-    talle: "43",
-    cantidad: 7,
-    color: "negro",
-    precio: 24000,
-}
-productos.push(calzado1) //7
-const calzado2 = {
-    nombre: 'Zapatillas Nike Jordan Low',
-    talle: "44",
-    cantidad: 10,
-    color: "blanco",
-    precio: 35000,
-}
-productos.push(calzado2) //8
-const calzado3 = {
-    nombre: 'Zapatillas Adidas Ultraboost',
-    talle: "40",
-    cantidad: 5,
-    color: "rojo",
-    precio: 26000,
-}
-productos.push(calzado3) //9
 
-console.log(productos)
+let prod1 = new Producto('001', 'Jordan Air Retro 11 Low ', 71000, './images/524979-150-auto.jfif')
+let prod2 = new Producto('002', 'Jordan Air XXXV', 32300, './images/588541-150-auto.jfif')
+let prod3 = new Producto('003', 'Jordan Air 1 Mid', 47500, './images/424881-150-auto.jfif')
+let prod4 = new Producto('004', 'Nike Air Force 1 GTX', 38400, './images/417890-150-auto.jfif')
+let prod5 = new Producto('005', 'Adidas Originals MD Black', 42000, './images/461204-150-auto.jfif')
+let prod6 = new Producto('006', 'Adidas Originals MD Grey', 42000, './images/574599-150-auto.jfif')
+let prod7 = new Producto('007', 'Adidas Originals MD Grey-Black', 42000, './images/607179-150-auto.jfif')
+let prod8 = new Producto('008', 'Adidas Originals Multix', 24000, './images/437270-150-auto.jfif')
 
-//Iteración de las opciones elegidas por el usuario utilizando ciclos condicionales y agregando cada opción elegida realizando push al carrito de compra.
-while (seguirComprando === true) {
-    if (productoSeleccionado === 1) {
-        let articulo = prompt("Estos son nuestros pantalones disponibles actualmente:\n\n1-" + pantalon1.nombre + ", $" + pantalon1.precio + "\n2-" + pantalon2.nombre + ", $" + pantalon2.precio + "\n3-" + pantalon3.nombre + ", $" + pantalon3.precio + "\n4-" + pantalon4.nombre + ", $" + pantalon4.precio + "\n\nElige cualquiera de estos productos ingresando el número correspondiente.")
-        if (articulo === "1") {
-            carrito.push(productos[0])
-        } else if (articulo === "2") {
-            carrito.push(productos[1])
-        } else if (articulo === "3") {
-            carrito.push(productos[2])
-        } else if (articulo === "4") {
-            carrito.push(productos[3])
-        } else {
-            productoSeleccionado = parseInt(prompt("Ingrese un numero existente: \n\n1-" + pantalon1.nombre + ", $" + pantalon1.precio + "\n2-" + pantalon2.nombre + ", $" + pantalon2.precio + "\n3-" + pantalon3.nombre + ", $" + pantalon3.precio + "\n4-" + pantalon4.nombre + ", $" + pantalon4.precio + "\n\n"))
-            continue
-        }
-    } else if (productoSeleccionado === 2) {
-        let articulo = prompt("Estas son nuestras remeras disponibles actualmente:\n\n1-" + remera1.nombre + ", $" + remera1.precio + "\n2-" + remera2.nombre + ", $" + remera2.precio + "\n3-" + remera3.nombre + ", $" + remera3.precio + "\n\nElige cualquiera de estos productos ingresando el número correspondiente.")
-        if (articulo === "1") {
-            carrito.push(productos[4])
-        } else if (articulo === "2") {
-            carrito.push(productos[5])
-        } else if (articulo === "3") {
-            carrito.push(productos[6])
-        } else {
-            productoSeleccionado = parseInt(prompt("Ingrese un numero existente: \n\n1-" + remera1.nombre + ", $" + remera1.precio + "\n2-" + remera2.nombre + ", $" + remera2.precio + "\n3-" + remera3.nombre + ", $" + remera3.precio + "\n\n"))
-            continue
-        }
-    } else if (productoSeleccionado === 3) {
-        let articulo = prompt("Este es nuestro calzado disponible actualmente:\n\n1-" + calzado1.nombre + ", $" + calzado1.precio + "\n2-" + calzado2.nombre + ", $" + calzado2.precio + "\n3-" + calzado3.nombre + ", $" + calzado3.precio + "\n\nElige cualquiera de estos productos ingresando el número correspondiente.")
-        if (articulo === "1") {
-            carrito.push(productos[7])
-        } else if (articulo === "2") {
-            carrito.push(productos[8])
-        } else if (articulo === "3") {
-            carrito.push(productos[9])
-        } else {
-            productoSeleccionado = parseInt(prompt("Ingrese un numero existente:\n\n1-" + calzado1.nombre + ", $" + calzado1.precio + "\n2-" + calzado2.nombre + ", $" + calzado2.precio + "\n3-" + calzado3.nombre + ", $" + calzado3.precio + "\n\n"))
-            continue
-        }
+productos.push(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8)
+
+productos.forEach(e => {
+    e.desplegarProductos()
+}) 
+productos.forEach(e => {
+    e.agregarEvento()
+})
+
+function agregarAlCarrito(producto) {
+
+    let enCarrito = carrito.find(prod => prod.id == producto.id)
+    
+    if(!enCarrito){
+        carrito.push({...producto, cantidad: 1})
     } else {
-        productoSeleccionado = parseInt(prompt("Ingresa un tipo de producto existente:\n\n1-Pantalones\n2-Remeras\n3-Calzado"))
-        continue
+        let carritoFiltrado = carrito.filter(prod => prod.id != producto.id)
+        carrito = [
+            ...carritoFiltrado, 
+            { ...enCarrito, cantidad: enCarrito.cantidad + 1}
+        ]
     }
 
-    decision = parseInt(prompt("Quieres seguir agregando productos?\n\n 1-Si \n 2-No"))
-    if (decision === 1) {
-        productoSeleccionado = parseInt(prompt("Ingresa el numero del producto que quieres comprar: \n\n1-Pantalones\n2-Remeras\n3-Calzado"))
-    } else if (decision === 2) {
-        seguirComprando = false
-    }
+    contador.innerHTML =  carrito.reduce((acc, prod) => acc + prod.cantidad, 0)
+    
 }
-
-console.log(carrito)
-//Iteración para generar la suma de los productos en el carrito de compra, y posteriormente mostrar al usuario a travez de un alert el total de la compra.
-for (let i = 0; i<carrito.length; i++) {
-    totalCompra = totalCompra + carrito[i].precio
-}
-
-alert("El valor total de su compra es de: $" + totalCompra + "\n\nGracias por comprar con nosotros " + nombreCliente + " ,que disfrutes tu compra!")
+const contador = document.getElementById('contadorCarrito')
+contador.innerHTML = carrito.reduce((acc, prod) => acc + prod.cantidad, 0)
